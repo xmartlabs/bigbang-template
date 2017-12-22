95f77f19e867384fae1f036190f78b400ea0870d
Matías Irland <matiasirland@gmail.com>
2017-12-22 13:09:10 -0300

    Add test template (#35)

---
+++ app/src/test/java/com/xmartlabs/template/module/MockAndroidModule.kt
@@ -0,0 +1,34 @@
+package com.xmartlabs.template.module
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.xmartlabs.bigbang.core.model.BuildInfo
+import com.xmartlabs.template.model.common.BuildInfo as CoreBuildInfo
+import com.xmartlabs.template.App
+import dagger.Module
+import dagger.Provides
+import org.mockito.Mockito
+import javax.inject.Singleton
+
+@Module
+class MockAndroidModule {
+  companion object {
+    internal val MOCK_CONTEXT = Mockito.mock(App::class.java)
+  }
+
+  @Provides
+  @Singleton
+  fun provideBuildInformation(): BuildInfo = CoreBuildInfo()
+
+  @Provides
+  @Singleton
+  fun provideApplicationContext(app: App): Context = app
+
+  @Provides
+  @Singleton
+  fun provideApplication(): App = MOCK_CONTEXT
+
+  @Provides
+  @Singleton
+  fun provideSharedPreferences(): SharedPreferences = Mockito.mock(SharedPreferences::class.java)
+}
---
+++ app/src/test/java/com/xmartlabs/template/tests/signin/SignInUnitTest.kt
@@ -0,0 +1,49 @@
+package com.xmartlabs.template.tests.signin
+
+import android.support.test.runner.AndroidJUnit4
+import com.xmartlabs.template.App
+import com.xmartlabs.template.R
+import com.xmartlabs.template.common.BaseUnitTest
+import com.xmartlabs.template.controller.AuthController
+import com.xmartlabs.template.service.AuthService
+import io.appflate.restmock.RESTMockServer
+import io.appflate.restmock.RequestsVerifier
+import io.appflate.restmock.utils.RequestMatchers
+import okhttp3.mockwebserver.MockResponse
+import org.hamcrest.core.AllOf.allOf
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+import javax.inject.Inject
+
+class SignInUnitTest : BaseUnitTest() {
+  companion object {
+    private val POST_ACCESS_TOKEN_MATCHER =
+        allOf(RequestMatchers.isPOST(), RequestMatchers.pathContains(AuthService.URL_ACCESS_TOKEN))
+  }
+
+  @Inject
+  internal lateinit var authController: AuthController
+
+  @Test
+  fun signInTest() {
+    authController.signIn().blockingAwait()
+  }
+
+  @Test
+  fun testAccessTokenOk() {
+    RESTMockServer.whenRequested(POST_ACCESS_TOKEN_MATCHER)
+        .thenReturn(MockResponse().setResponseCode(411))
+
+    authController.accessToken.toCompletable()
+        .onErrorComplete()
+        .blockingAwait()
+
+    RequestsVerifier.verifyRequest(POST_ACCESS_TOKEN_MATCHER).invoked()
+  }
+
+  @Test
+  fun testMockString() {
+    Assert.assertEquals(DEFAULT_MOCK_STRING, App.context.getString(R.string.app_name))
+  }
+}
