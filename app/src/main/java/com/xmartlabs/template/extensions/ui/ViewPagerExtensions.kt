e48985f8ab0eb842448aed2114149f9031d31baa
Matías Irland <mirland@xmartlabs.com>
2018-08-15 14:49:10 -0300

    Fix some comments

--- app/src/main/java/com/xmartlabs/template/extensions/ui/ViewPagerExtensions.kt
+++ app/src/main/java/com/xmartlabs/template/extensions/ui/ViewPagerExtensions.kt
@@ -1,15 +1,13 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package com.xmartlabs.template.extensions.ui
 
 import android.support.v4.view.ViewPager
 import com.xmartlabs.template.helper.EmptyOnPageChangeListener
 
-inline fun ViewPager.nextPage() {
+fun ViewPager.nextPage() {
   currentItem++
 }
 
-inline fun ViewPager.isLastPage() = currentItem == (adapter?.let { adapter -> adapter.count - 1 })
+fun ViewPager.isLastPage() = currentItem == (adapter?.let { adapter -> adapter.count - 1 })
 
 inline fun ViewPager.onPageSelected(crossinline action: (position: Int) -> Unit) {
   addOnPageChangeListener(object : EmptyOnPageChangeListener() {
